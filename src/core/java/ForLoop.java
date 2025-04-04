b50ff5e1df0433a7e6e06d6b329da619da631541
Misskarina <karinakumari907ce@gmail.com>
2025-04-04 22:41:52 +0530

    For Loop

---
+++ src/core/java/ForLoop.java
@@ -0,0 +1,15 @@
+package core.java;
+
+/**
+ *
+ * @author MISS KARINA
+ */
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+
