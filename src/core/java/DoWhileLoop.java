9ee20c9f3b4f80219bf3245777baf5a4377bca8a
Misskarina <karinakumari907ce@gmail.com>
2025-04-04 22:43:45 +0530

    Do While Loop

---
+++ src/core/java/DoWhileLoop.java
@@ -0,0 +1,17 @@
+package core.java;
+
+/**
+ *
+ * @author MISS KARINA
+ */
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 50);
+    }
+}
+
+
