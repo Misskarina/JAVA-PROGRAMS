643b94150a6fa82ed7d7d7b468392a9fc1c6d12d
Misskarina <karinakumari907ce@gmail.com>
2025-04-13 12:43:40 +0530

    Boolean Values

---
+++ src/core/java/Boolean.java
@@ -0,0 +1,18 @@
+package core.java;
+
+/**
+ *
+ * @author MISS KARINA
+ */
+public class Boolean {
+  public static void main(String[] args) {
+    int myAge = 25;
+    int votingAge = 18;
+    
+    if (myAge >= votingAge) {
+      System.out.println("Eligible to vote!");
+    } else {
+      System.out.println("Not eligible to vote.");
+    }  
+  }
+}
